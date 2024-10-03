afe2445baa0b2f06cb02225ca3ceb18f85aa05ff
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-10-03 13:11:55 +0500

    Adding fallback for loading state

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -5,14 +5,17 @@ import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   // TODO: fetch available places from backend API
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
 
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
 
     // fetch("http://localhost:3000/places")
@@ -30,6 +33,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching places data ..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
