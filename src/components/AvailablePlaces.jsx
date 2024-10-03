cb8f3a9fca6a1dd80b032368ab398874a73cf712
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-10-03 11:36:03 +0500

    Fetch data using fetch() method

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,24 @@
-import Places from './Places.jsx';
+import { useState } from "react";
+import Places from "./Places.jsx";
+
+// const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  // TODO: fetch available places from backend API
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  fetch("http://localhost:3000/places")
+    .then((response) => {
+      return response.json();
+    })
+    .then((resData) => {
+      setAvailablePlaces(resData.places);
+    });
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
