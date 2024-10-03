f7d3f171efca0e26e555060c9a7c9095ef9bc7ba
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-10-03 14:14:26 +0500

    Sorting Places accoring to user's current coordinates

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react"
-;
+import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import ErrorPage from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 // const places = localStorage.getItem('places');
 
@@ -12,7 +12,6 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [error, setError] = useState();
 
   useEffect(() => {
-
     async function fetchPlaces() {
       setIsFetching(true);
 
@@ -21,33 +20,32 @@ export default function AvailablePlaces({ onSelectPlace }) {
         const resData = await response.json();
 
         if (!response.ok) {
-          // const error = new Error('Failed to fetch places');
-          // throw error;
-
-          throw new Error('Failed to fetch places')
+          throw new Error("Failed to fetch places");
         }
 
-      setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
-        setError({ message: error.message || 'Counld not fetch places, please try again later' });
+        setError({
+          message:
+            error.message || "Counld not fetch places, please try again later",
+        });
+        setIsFetching(false);
       }
-      
-      setIsFetching(false);
     }
-
-    // fetch("http://localhost:3000/places")
-    //   .then((response) => {
-    //     return response.json();
-    //   })
-    //   .then((resData) => {
-    //     setAvailablePlaces(resData.places);
-    //   });
 
     fetchPlaces();
   }, []);
 
   if (error) {
-    return <ErrorPage title="An error occured" message={error.message}/>
+    return <ErrorPage title="An error occured" message={error.message} />;
   }
 
   return (
